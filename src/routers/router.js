759fe2d06de91da5d58cc4f5c1eb410c4cc697cf
mineozkn <97402144+mineozkn@users.noreply.github.com>
2022-01-24 10:50:42 +0530

    Update router.js

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = function(app) {
   var controller = require('../controllers/controller');
-
+  
+  // check app health
+  app.route('/health')
+    .get(controller.health);
+  
   // todoList Routes
   app.route('/records')
     .post(controller.records);
